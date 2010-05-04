bf5d07ff1906f026f7181971519dffa8b3ea7379
Chris Gibson <cgibson@mrvoxel.com>
2010-05-04 22:48:45 +0000

    added functionality to quaternions

--- src/math/quaternion.h
+++ src/math/quaternion.h
@@ -1,10 +1,21 @@
 #ifndef _QUATERNION_H_
 #define _QUATERNION_H_
 
+#include "../helper/Vector.h"
+
 class Quaternion {
 public:
   Quaternion();
   Quaternion(float h, float i, float j, float k);
+  
+  Vector getAxis();
+  void setAxis(Vector axis);
+  float getAngle();
+  void setAngle(float angle);
+  float getH();
+  float getI();
+  float getJ();
+  float getK();
 private:
   float m_h, m_i, m_j, m_k;
 };

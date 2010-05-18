5160a9d377c05a913b35755e17179f8d3f95cdc2
Chris Gibson <cgibson@mrvoxel.com>
2010-05-18 07:05:24 +0000

    added materials.cpp. also added temporary debug code for david

--- src/graphics/Material.h
+++ src/graphics/Material.h
@@ -17,64 +17,15 @@ class Material {
     GLfloat specular[4];
     GLfloat shininess;
     
-    Material(GLfloat in_ambient[4], GLfloat in_diffuse[4], GLfloat in_specular[4], GLfloat in_shininess[1])
-    {
-      int i;
-      for(i = 0; i < 4; i++)
-      {
-        ambient[i] = in_ambient[i];
-        diffuse[i] = in_diffuse[i];
-        specular[i] = in_specular[i];
-      }
-      shininess = in_shininess[0];
-    }
+    Material(GLfloat in_ambient[4], GLfloat in_diffuse[4], GLfloat in_specular[4], GLfloat in_shininess[1]);
     
-    void applyMaterial(GLint program, const char *uniform)
-    {
-      glUseProgram(program);
-      int loc = glGetUniformLocation(program, "material.ambient");
-      glUniform4fv(loc, 1, ambient);
-      loc = glGetUniformLocation(program, "material.diffuse");
-      glUniform4fv(loc, 1, diffuse);
-      loc = glGetUniformLocation(program, "material.specular");
-      glUniform4fv(loc, 1, specular);
-      loc = glGetUniformLocation(program, "material.shininess");
-      glUniform1f(loc, shininess);
-    }
+    void applyMaterial(GLint program, const char *uniform);
     
-    void applyMaterial(int program, int location) {
-      //glUniform4fv(location, 4, const_cast<GLfloat*>(data));
-    }
+    void applyMaterial(int program, int location);
     
     void interpolateFloats(GLfloat *list1, GLfloat *list2, 
-                           GLfloat **ret, int count, float t)
-    {
-      float diff;
-      for(int i = 0; i < count; i++)
-      {
-        diff = list2[i] - list1[i];
-        (*ret)[i] = list1[i] + diff * t;
-      }
-    }
+                           GLfloat **ret, int count, float t);
     
-    /*Material interpolate(Material other, float t)
-    {
-      Material res;
-      GLfloat *rAmbient = (GLfloat *)(res.ambient);
-      GLfloat *rDiffuse = (GLfloat *)(res.diffuse);
-      GLfloat *rSpecular = (GLfloat *)(res.specular);
-      GLfloat *rShininess = (GLfloat *)(res.shininess);
-      interpolateFloats((GLfloat*)ambient, (GLfloat*)other.ambient, 
-                        (GLfloat**)&rAmbient, 4, t);
-      interpolateFloats((GLfloat*)diffuse, (GLfloat*)other.diffuse, 
-                        (GLfloat**)&rDiffuse, 4, t);
-      interpolateFloats((GLfloat*)specular, (GLfloat*)other.specular, 
-                        (GLfloat**)&rSpecular, 4, t);
-      interpolateFloats((GLfloat*)shininess, (GLfloat*)other.shininess, 
-                        (GLfloat**)&rShininess, 1, t);
-      
-      return res;
-    }*/
     Material(){};
 private:
 };
